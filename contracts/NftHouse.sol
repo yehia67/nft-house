69a58b728850aaf72bd577824f1df37531ef6adb
Yehia Tarek <yehia@startfi.io>
2022-01-14 21:23:25 +0200

    add mint NFT house

--- contracts/NftHouse.sol
+++ contracts/NftHouse.sol
@@ -2,18 +2,34 @@
 pragma solidity ^0.7.6;
 
 import './superfluid/TradeableCashflow.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 
 contract NftHouse is TradeableCashflow {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdTracker;
+
     struct HouseInfo {
-        string tokenId;
+        uint256 tokenId;
         address owner;
-        address[] renters;
+        uint256 numberOfRenters;
         uint256 rentPrice;
         uint256 sellingPrice; // if equal zero the house is not for sale
     }
+
+    HouseInfo[] public houses;
+
     mapping(address => HouseInfo) public isOwner;
     mapping(address => HouseInfo) public isRenter;
     mapping(address => uint256) public lastTimePaid;
+
+    event HouseMinted(
+        address indexed _owner,
+        uint256 indexed _tokenId,
+        string tokenUri,
+        uint256 _rentPrice,
+        uint256 _sellingPrice
+    );
 
     constructor(
         string memory _name,
@@ -21,6 +37,31 @@ contract NftHouse is TradeableCashflow {
         ISuperfluid host,
         IConstantFlowAgreementV1 cfa,
         ISuperToken acceptedToken
-    ) TradeableCashflow( _msgSender(), _name,_symbol,host,cfa,acceptedToken) {}
+    ) TradeableCashflow(_msgSender(), _name, _symbol, host, cfa, acceptedToken) {}
 
+    function mintHouse(
+        string memory tokenUri,
+        uint256 numberOfRenters,
+        uint256 rentPrice,
+        uint256 sellingPrice
+    ) external returns (uint256) {
+        _mint(_msgSender(), _tokenIdTracker.current());
+        _setTokenURI(_tokenIdTracker.current(), tokenUri);
+
+        HouseInfo memory house = HouseInfo({
+            tokenId: _tokenIdTracker.current(),
+            owner: _msgSender(),
+            numberOfRenters: numberOfRenters,
+            rentPrice: rentPrice,
+            sellingPrice: sellingPrice
+        });
+
+        houses.push(house);
+        isOwner[_msgSender()] = house;
+
+        emit HouseMinted(_msgSender(), _tokenIdTracker.current(), tokenUri, rentPrice, sellingPrice);
+
+        _tokenIdTracker.increment();
+        return _tokenIdTracker.current();
+    }
 }
