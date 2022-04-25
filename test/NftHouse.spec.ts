249f6d4612f0d7cd1857eab3467fec7e464f2dfe
Yehia Tarek <yehia@startfi.io>
2022-04-25 21:53:01 +0200

    add unit test cases for users to pay rent and sell/buy houses

--- test/NftHouse.spec.ts
+++ test/NftHouse.spec.ts
@@ -1,4 +1,4 @@
-import { parseEther } from '@ethersproject/units'
+import { formatEther, parseEther } from '@ethersproject/units'
 import chai, { expect } from 'chai'
 import { Contract } from 'ethers'
 import { waffle } from 'hardhat'
@@ -7,41 +7,48 @@ const { solidity, deployContract, provider } = waffle
 import NftHouse from '../artifacts/contracts/NftHouse.sol/NftHouse.json'
 
 chai.use(solidity)
-const nftName= "House NFT"
-const symbol = "HNFT"
-describe('Nft House Unit Test', () => {
-  const [deployer, user1, user2] = provider.getWallets()
+const nftName = 'House NFT'
+const symbol = 'HNFT'
+const rentPrice = parseEther('0.06')
+const sellingPrice = parseEther('0')
+describe('Rent house for two people', () => {
+  const [deployer, user1, user2, user3] = provider.getWallets()
 
   let rentContract: Contract
 
   before(async () => {
-    rentContract = await deployContract(deployer, NftHouse, [nftName,symbol])
+    rentContract = await deployContract(deployer, NftHouse, [nftName, symbol])
   })
 
-  it('Should mint five houses', async () => {
-    await expect(rentContract.mintHouse('ipfs://uri-1', 1, parseEther('2'), parseEther('0'))).to.emit(
-      rentContract,
-      'HouseMinted'
-    )
-    await expect(rentContract.connect(user1).mintHouse('ipfs://uri-1', 3, parseEther('3'), parseEther('0'))).to.emit(
-      rentContract,
-      'HouseMinted'
-    )
-    await expect(rentContract.connect(user2).mintHouse('ipfs://uri-1', 4, parseEther('4'), parseEther('100'))).to.emit(
-      rentContract,
-      'HouseMinted'
-    )
-    await expect(rentContract.connect(user2).mintHouse('ipfs://uri-1', 1, parseEther('21'), parseEther('0.06'))).to.emit(
-      rentContract,
-      'HouseMinted'
-    )
-    await expect(rentContract.connect(user1).mintHouse('ipfs://uri-1', 1, parseEther('87'), parseEther('0.001'))).to.emit(
+  it('Mint an NFT for house with two renters and without selling option', async () => {
+    await expect(rentContract.mintHouse('ipfs://uri-1', 2, rentPrice, sellingPrice)).to.emit(
       rentContract,
       'HouseMinted'
     )
   })
 
- it('Should mint five houses', async () => {
-   
+  it('User one should pay rent to be a renter', async () => {
+    await expect(rentContract.connect(user1).payRent(0, { value: rentPrice })).to.emit(rentContract, 'PayRent')
+  })
+
+  it('User two should pay rent to be a renter', async () => {
+    await expect(rentContract.connect(user2).payRent(0, { value: rentPrice })).to.emit(rentContract, 'PayRent')
+  })
+
+  it('User three is not a renter and transaction should be reverted', async () => {
+    await expect(rentContract.connect(user3).payRent(0, { value: rentPrice })).to.be.revertedWith(
+      'You are not a renter for this house'
+    )
+  })
+
+  it('Owner should offer his house for sale', async () => {
+    const sellPrice = parseEther('1')
+    await rentContract.approve(rentContract.address, 0)
+    await expect(rentContract.sell(0, sellPrice)).to.emit(rentContract, 'Sold')
+  })
+
+  it('User One should buy the offered house for sale', async () => {
+    const buyPrice = parseEther('1')
+    await expect(rentContract.buy(0, { value: buyPrice })).to.emit(rentContract, 'Bought')
   })
 })
